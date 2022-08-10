d31016cac0a430376862914f1f6fc858aedab28c
Maksym Chuprun <m307837@alunos.ipb.pt>
2022-08-10 02:03:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { CarsService } from './services/cars.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http-project';
+export class AppComponent implements OnInit {
+
+  carsData = []
+
+  constructor(private carsService: CarsService) {}
+  
+  ngOnInit(){
+    this.carsService
+    .getCars()
+    .subscribe((res) => {
+      console.log(res);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarsService } from './services/cars.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
